a8f0b1306521866df695b18a1a761fcbf113b0ed
meocutee123 <nghia241717378@gmail.com>
2020-11-29 23:00:45 +0700

    add header and footer

---
+++ templates/js/common/login.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".login-bnt");
+const x = document.querySelector(".login");
+
+function LoginFormOpen(){
+    x.style.right = "0";
+}
+function LoginFormClose(){
+    x.style.right = "-35vw";
+}
